6f512990a7f5bffec4985add905bf39c72c88a00
Keiji-morita <keijim3116@gmail.com>
2022-05-03 14:24:58 +0900

    リンクデザイン中

--- components/intro_nav.js
+++ components/intro_nav.js
@@ -1,33 +1,34 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 import "tailwindcss/tailwind.css";
 
-import { Container } from 'postcss'
+import { Container } from "postcss";
 
 export default function Intro() {
   return (
-    <><header>
-
-    </header>
-    <div class="h-2/3 w-screen">
-    <div class="  flex flex-row h-3/4 " >
-        <div class="basis-2/4  ">
-          <div class="p-8 max-w-3xl text-5xl md:text-6xl font-bold mx-auto dark:text-white text-gray-800 text-center mt-auto">
-          <h3 class="font-mono  text-2xl ">
-            Hello!
-          </h3>
-          <h3 class="font-mono  text-2xl">Nice to meet you!</h3>
+    <>
+      <header></header>
+      <div class="h-2/3 w-screen">
+        <div class="  flex flex-row h-3/4 ">
+          <div class="basis-2/4 ">
+            <div class="p-8 max-w-3xl text-5xl md:text-6xl font-bold mx-auto dark:text-white text-gray-800 text-center mt-auto place-content-between">
+              <h3 class="font-mono  text-2xl ">Hello!</h3>
+              <h3 class="font-mono  text-2xl">Nice to meet you!</h3>
+              <h1 class="font-mono my-16">Keiji Morita</h1>
+            </div>
           </div>
 
-          <h1 class="font-mono ">Keiji Morita</h1>
+          <img
+            class="rounded-full basis-1/4 h-67 w-60"
+            src="/images/brian.png"
+            alt="My avatar"
+          />
         </div>
-
-
-        <img class="rounded-full basis-1/4 h-67 w-60" src="/images/brian.png" alt="My avatar" />
-      </div>
-      <div class="border-double">
-        <nav list-none class="  relative
+        <div class="border-double">
+          <nav
+            list-none
+            class="  relative
   w-3/4
   flex flex-wrap
   items-center
@@ -38,23 +39,45 @@ export default function Intro() {
   hover:text-gray-700
   focus:text-gray-700
   shadow-lg
-  navbar navbar-expand-lg navbar-light container mx-auto">
-          
+  navbar navbar-expand-lg navbar-light container mx-auto"
+          >
             <div class="flex flex-row container mx-auto text-center">
               <div>
-              <a href="/posts/profile" class="py-2 px-6 flex font-mono  hover:text-blue-600/75">Profile</a>
+                <a
+                  href="/posts/profile"
+                  class="py-2 px-6 flex font-mono  hover:text-blue-600/75"
+                >
+                  Profile
+                </a>
               </div>
               <div>
-              <a href="/posts/products" class="py-2 px-6 flex font-mono  hover:text-blue-600/75">Products</a>
+                <a
+                  href="/posts/products"
+                  class="py-2 px-6 flex font-mono  hover:text-blue-600/75"
+                >
+                  Products
+                </a>
               </div>
               <div>
-              <a href="/posts/contact" class="py-2 px-6 flex font-mono  hover:text-blue-600/75">Contact</a>
+                <a
+                  href="/posts/contact"
+                  class="py-2 px-6 flex font-mono  hover:text-blue-600/75"
+                >
+                  Contact
+                </a>
+              </div>
+              <div>
+                <a
+                  href="/posts/contact"
+                  class="py-2 px-6 flex font-mono  hover:text-blue-600/75"
+                >
+                  Plan
+                </a>
               </div>
             </div>
-        </nav>
+          </nav>
+        </div>
       </div>
-      </div>
-      </>
-
-  )
+    </>
+  );
 }
---
+++ components/resume.js
@@ -0,0 +1,43 @@
+import "tailwindcss/tailwind.css";
+
+
+export default function resume() {
+  return
+  
+  <table class="rounded-t-lg m-5 w-5/6 mx-auto bg-gray-200 text-gray-800">
+        <thead>
+          {/* <tr>
+            <th class="border">When</th>
+            <th class="border"> Event</th>
+          </tr> */}
+        </thead>
+
+        <tbody>
+          <tr class="text-left border-b-2 border-gray-300">
+            <td class="border">2002.10.21</td>
+            <td class="border">神奈川県生</td>
+          </tr>
+
+          <tr class="text-left border-b-2 border-gray-300">
+            <td class="border">2021.4</td>
+            <td class="border">関西学院大学　商学部入学</td>
+          </tr>
+
+          <tr class="text-left border-b-2 border-gray-300">
+            <td class="border">2021.6</td>
+            <td class="border">関西学院大学　プログラミング研究会　Tech.uniへ入会</td>
+          </tr>
+
+          <tr class="text-left border-b-2 border-gray-300">
+            <td class="border">2022.3</td>
+            <td class="border">Geek salon内でのハッカソンでオーディエンス賞受賞</td>
+          </tr>
+
+          <tr class="text-left border-b-2 border-gray-300">
+            <td class="border">2025.3</td>
+            <td class="border">同学卒業見込み</td>
+          </tr>
+        </tbody>
+      </table>
+  
+}
